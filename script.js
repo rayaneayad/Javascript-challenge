c23b3de2de8447f5f4351154343b8a7b006d4586
rayaneayd <raysayd23@gmail.com>
2022-02-28 15:50:29 +0100

    Javascript Challenge 4

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-const bernardTaille1 = (1.69);
-const bernardTaille2 = (1.88);
-const bernardPoids1 = (78);
-const bernardPoids2 = (95);
-const marcelTaille1 = (1.88);
-const marcelTaille2 = (1.76);
-const marcelPoids1 = (95);
-const marcelPoids2 = (85);
 
-const bernardImc = bernardPoids1 / (bernardTaille1*bernardTaille1);
-const bernardImc2 = bernardPoids2 / (bernardTaille2*bernardTaille2);
-const marcelImc = marcelPoids1 / (marcelTaille1*marcelTaille1);
-const marcelImc2 = marcelPoids2 / (marcelTaille2*marcelTaille2);
+let moyenneDauphins1 = (96 + 108 + 89) / 3;
+let moyenneDauphins2 = (97 + 112 + 101) / 3;
+let moyenneDauphins3 = (97 + 112 + 101) / 3;
+let moyenneKoalas1 = (88 + 91 + 110) / 3;
+let moyenneKoalas2 = (109 + 95 + 123) / 3;
+let moyenneKoalas3 = (109 + 95 + 106) / 3;
+
+const resultDauphins = (moyenneDauphins1 + moyenneDauphins2 + moyenneDauphins3) /3;
+const resultKoalas = (moyenneKoalas1 + moyenneKoalas2 + moyenneKoalas3) /3;
+
+const winD = resultDauphins > resultDauphins;
+const winK = resultKoalas > resultDauphins;
+
+if (resultDauphins < 100  || resultKoalas < 100)
+{
+  document.write("Pas de gagnant.")
+}
 
 
-
-if (bernardHigherIMC = bernardImc > marcelImc ){
-  document.write("Bernard a un IMC" + " (" + bernardImc  + ") " + "plus élevé que Marcel" + " (" + marcelImc + ").");
+if (winD){
+  document.write("Les dauphins sont vainceurs !!!");
 }
 
 else {
-  document.write("Marcel a un IMC plus élevé que Bernard");
+  document.write("Les Koalas sont vainceurs !!!");
+  document.write('<br/>');
+  document.write(parseInt(resultDauphins) + (' - ') + parseInt(resultKoalas));
 }
 
-if(bernardImc < 28 || bernardImc2 < 28){
-  document.write('<br/>');
-  document.write("Bernard : insuffisance pondérale.");
-}
-
-if(marcelImc > 25 || bernardImc2 > 25){
-  document.write('<br/>');
-  document.write("Marcel : Surpoids.");
-}
 
